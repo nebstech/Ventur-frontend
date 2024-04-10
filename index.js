5550a4422180d5d7d5e112887345622299f982a6
Adonis <adonis.casado@outlook.com>
2024-04-10 11:30:54 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const config = {
+  cUrl: 'https://www.universal-tutorial.com/api/countries/'
+  
+}
+
+// const toggleAddLocation = () => {
+//   const addLocationSection = document.getElementById('addLocationSection');
+//   if (isNewTrip) {
+//     addLocationSection.style.display = 'block';
+//   } else {
+//     addLocationSection.style.display = 'none';
+//   }
+// };
+
+// toggleAddLocation();
+
